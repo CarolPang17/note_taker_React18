8463e8546f983588b80ad01ab46cdf9a9fd9bb7c
carolpang <carolpang17@gmail.com>
2022-07-12 10:49:59 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+
+//create a react element
+const App = <div> Hello React </div>
+
+//get root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(App);
