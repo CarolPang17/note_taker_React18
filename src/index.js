c55640db39022df97d5929852fa33db317e74e25
carolpang <carolpang17@gmail.com>
2022-07-12 11:44:53 -0700

    create first log's content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import ReactDOM from 'react-dom/client';
 
 //create a react element
-const App = <div> Hello React !! </div>
+const App = (
+  <div className="logs">
+    {/*div of each logs*/}
+    <div className="item"></div>
+    <div className="date"></div>
+    <div className="month">April</div>
+    <div className="day">19</div>
+    {/* content of each log */}
+    <div className='content'>
+      <h2 className='desc'> Learning Reach </h2>
+      <div className='time'> 40 mins </div>
+    </div>
+  </div>
+);
 
 //get root element
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(App);
