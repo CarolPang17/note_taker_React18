6b64a84fc6167709038524ec294b1c0406f794b6
carolpang <carolpang17@gmail.com>
2022-07-13 10:46:54 -0700

    created css

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import ReactDOM from 'react-dom/client';
 
+//import the stylesheet
+import './index.css';
+
 //create a react element
 const App = (
   <div className="logs">
     {/*div of each logs*/}
-    <div className="item"></div>
-    <div className="date"></div>
-    <div className="month">April</div>
-    <div className="day">19</div>
-    {/* content of each log */}
-    <div className='content'>
-      <h2 className='desc'> Learning Reach </h2>
-      <div className='time'> 40 mins </div>
+    <div className="item">
+      <div className="date">
+        <div className="month">April</div>
+        <div className="day">19</div>
+      </div>
+      {/* content of each log */}
+      <div className="content">
+        <h2 className="desc"> Learning Reach </h2>
+        <div className="time"> 40 mins </div>
+      </div>
     </div>
+
+{/* item 2 */}
+    <div className="item">
+      <div className="date">
+        <div className="month">April</div>
+        <div className="day">19</div>
+      </div>
+      {/* content of each log */}
+      <div className="content">
+        <h2 className="desc"> Learning Reach </h2>
+        <div className="time"> 40 mins </div>
+      </div>
+    </div>
+
+{/* item 2 end */}
   </div>
 );
 
